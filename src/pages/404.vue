7f75d9946a152eeb18fdc2aa1f0a848cee2c7d6a
Linh Nguyen <tuleism@gmail.com>
2020-08-29 14:55:08 +0800

    proper 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,12 @@
+<template>
+  <Layout :sidebar="true">
+    <div class="content">
+      <h1>404 Teapot Not Found</h1>
+      <h2>Here is a random image for your entertainment</h2>
+      <img src="https://source.unsplash.com/random/800x600"/>
+    </div>
+  </Layout>
+</template>
+
+<script>
+</script>
